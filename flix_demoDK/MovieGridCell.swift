d2ecb1187d5d508830826262859cff7b44c15d4d
Debarshi Kundu <debarshikundu0@gmail.com>
2019-02-17 19:49:48 -0600

    Added collection view. Reorganized files. Built in tabs.

---
+++ flix_demoDK/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix_demoDK
+//
+//  Created by Debarshi Kundu on 2/17/19.
+//  Copyright © 2019 Kathie Inc. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
